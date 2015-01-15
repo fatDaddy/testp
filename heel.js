6cf3df948dedd6f7984bc3e193a704d06aeaf70c
fatDaddy <chairoverlord@yahoo.com>
2015-01-14 16:26:31 -0800

    edit via cmd

--- heel.js
+++ heel.js
@@ -2,13 +2,6 @@ public class hellow
 {
 	public static void main(String [] args)
 	{
-		if(1+2 == 3)
-		{
-			system.out.println("faD");
-		}
-		else
-		{
-			system.out.println("popopop");
-		}
+		system.out.print("partypooper");
 	}
 }
